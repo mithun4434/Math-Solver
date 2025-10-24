3d7dde9ec62ae695a5891f91cc368ceea221d5b7
mithun4434 <96248479+mithun4434@users.noreply.github.com>
2025-10-24 17:52:07 +0530

    Add files via upload

---
+++ SparklesIcon.tsx
@@ -0,0 +1,10 @@
+
+import React from 'react';
+
+const SparklesIcon: React.FC = () => (
+    <svg xmlns="http://www.w3.org/2000/svg" className="h-8 w-8 text-white" viewBox="0 0 24 24" fill="currentColor">
+        <path fillRule="evenodd" d="M12.964 2.133a.75.75 0 00-1.028-.999L2.83 6.365a.75.75 0 00.52 1.344l5.356-.669 1.575 4.897L6.9 13.563a.75.75 0 00.401 1.383l4.755-1.426 2.82 2.82a.75.75 0 001.06-1.06l-2.82-2.82 4.754-1.426a.75.75 0 00.402-1.383L14.4 11.936l1.575-4.897 5.356.67a.75.75 0 00.52-1.345L12.964 2.133zM18 15.5a2.5 2.5 0 11-5 0 2.5 2.5 0 015 0zM6.5 21a2.5 2.5 0 100-5 2.5 2.5 0 000 5z" clipRule="evenodd" />
+    </svg>
+);
+
+export default SparklesIcon;
